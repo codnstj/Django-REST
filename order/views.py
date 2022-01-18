a7fe4a4074b78be8e4690c167100dce34600d971
lot8229 <lot8229@kakao.com>
2022-01-18 18:02:38 +0900

    link templalate & menu api

--- order/views.py
+++ order/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from order.models import Shop, Menu, Order, Order_food
-from order.serializer import ShopSerializer
+from order.serializer import ShopSerializer, MenuSerializer
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -10,8 +10,7 @@ from rest_framework.parsers import JSONParser
 def shop(request):
     if request.method == 'GET':
         shop = Shop.objects.all()
-        serializer = ShopSerializer(shop, many=True)  # 데이터를 json 파일 의 형태로 바꿔준다.
-        return JsonResponse(serializer.data, safe=False)  # json 파일로 바꿔 준 데이터를 Response 한다.
+        return render(request,'order/shop_list.html',{'shop_list':shop})
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ShopSerializer(data=data)
@@ -19,14 +18,17 @@ def shop(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-def menu(request):
+
+
+@csrf_exempt  # 보안 적인 것,가로채기안되게끔
+def menu(request , shop):
     if request.method == 'GET':
-        shop = Shop.objects.all()
-        serializer = ShopSerializer(shop, many=True)  # 데이터를 json 파일 의 형태로 바꿔준다.
-        return JsonResponse(serializer.data, safe=False)  # json 파일로 바꿔 준 데이터를 Response 한다.
+        menu = Menu.objects.filter(Shop=shop)
+        serializer = MenuSerializer(menu, many=True)  # 데이터를 json 파일 의 형태로 바꿔준다.
+        return render(request,'order/menu_list.html',{'menu_list':menu})
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = ShopSerializer(data=data)
+        serializer = MenuSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
