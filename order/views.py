88565a93a8100a988038b3ea206672d2108cf200
lot8229 <lot8229@kakao.com>
2022-01-18 17:18:21 +0900

    organize order

--- order/views.py
+++ order/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from order.models import Shop, Menu, Order, Order_food
+from order.serializer import ShopSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+
+@csrf_exempt  # 보안 적인 것,가로채기안되게끔
+def shop(request):
+    if request.method == 'GET':
+        shop = Shop.objects.all()
+        serializer = ShopSerializer(shop, many=True)  # 데이터를 json 파일 의 형태로 바꿔준다.
+        return JsonResponse(serializer.data, safe=False)  # json 파일로 바꿔 준 데이터를 Response 한다.
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+def menu(request):
+    if request.method == 'GET':
+        shop = Shop.objects.all()
+        serializer = ShopSerializer(shop, many=True)  # 데이터를 json 파일 의 형태로 바꿔준다.
+        return JsonResponse(serializer.data, safe=False)  # json 파일로 바꿔 준 데이터를 Response 한다.
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
